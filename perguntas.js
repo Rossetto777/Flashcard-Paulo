5d68534ab8bac1bb8f87f5ecd5e990e78fb09ca1
Rossetto777 <166571382+Rossetto777@users.noreply.github.com>
2024-11-05 22:37:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    "futebol",
+    'quem é o camisa 10 do Brasil?',
+    'Neymar'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o destrito de São Pedro do Ívai?',
+    'Mariza'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
